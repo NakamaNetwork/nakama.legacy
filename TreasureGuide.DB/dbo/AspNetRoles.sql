4af26844472f4830ba3e3ccb604c572547b15379
Bobby Cafazzo <robocafaz@gmail.com>
2016-12-29 19:23:04 -0500

    Adding SQL project and user login data.

---
+++ TreasureGuide.DB/dbo/AspNetRoles.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[AspNetRoles](
+	[Id] [nvarchar](128) NOT NULL,
+	[Name] [nvarchar](256) NOT NULL,
+ CONSTRAINT [PK_dbo.AspNetRoles] PRIMARY KEY CLUSTERED ([Id] ASC)
+)
---
+++ TreasureGuide.DB/dbo/AspNetUserRoles.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[AspNetUserRoles](
+	[UserId] [nvarchar](128) NOT NULL,
+	[RoleId] [nvarchar](128) NOT NULL,
+ CONSTRAINT [PK_dbo.AspNetUserRoles] PRIMARY KEY CLUSTERED ([UserId] ASC, [RoleId] ASC),
+ CONSTRAINT [FK_dbo.AspNetUserRoles_dbo.AspNetRoles_RoleId] FOREIGN KEY([RoleId]) REFERENCES [dbo].[AspNetRoles] ([Id]) ON DELETE CASCADE
+)
