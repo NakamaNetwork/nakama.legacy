ff0bb205b0b712aace1126b56bcc4580733e297f
BCAFAZZO-LAPTOP\roboc <bcafazzo@nbi.company>
2017-01-04 14:14:05 -0500

    Adding stage entries

---
+++ Source/TreasureGuide.DB/dbo/Stages.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Stages]
+(
+	[Id] INT NOT NULL,
+    [Name] NVARCHAR(500) NULL,
+	[Type] TINYINT NOT NULL CONSTRAINT [DF_dbo.Stages_Type] DEFAULT 0,
+	[Global] BIT NOT NULL CONSTRAINT [DF_dbo.Stages_Global] DEFAULT 1,
+    CONSTRAINT [PK_dbo.Stages] PRIMARY KEY CLUSTERED ([Id] ASC)
+)
