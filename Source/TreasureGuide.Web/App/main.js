b126ca89457d387e58d4aa1c5f6bf8b2477d609a
Bobby Cafazzo <robocafaz@gmail.com>
2017-01-12 21:21:11 -0500

    Adding durandal starter kit to project.

---
+++ Source/TreasureGuide.Web/App/main.js
@@ -0,0 +1,34 @@
+﻿requirejs.config({
+    paths: {
+        'text': '../Scripts/text',
+        'durandal': '../Scripts/durandal',
+        'plugins': '../Scripts/durandal/plugins',
+        'transitions': '../Scripts/durandal/transitions',
+        'bootstrap':'../Scripts/bootstrap'
+    }
+});
+
+define('jquery', function () { return jQuery; });
+define('knockout', ko);
+
+define(['durandal/system', 'durandal/app', 'durandal/viewLocator', 'bootstrap'], function (system, app, viewLocator) {
+    //>>excludeStart("build", true);
+    system.debug(true);
+    //>>excludeEnd("build");
+
+    app.title = 'Durandal Starter Kit';
+
+    app.configurePlugins({
+        router: true,
+        dialog: true
+    });
+
+    app.start().then(function() {
+        //Replace 'viewmodels' in the moduleId with 'views' to locate the view.
+        //Look for partial views in a 'views' folder in the root.
+        viewLocator.useConvention();
+
+        //Show the app by setting the root view model for our application with a transition.
+        app.setRoot('viewmodels/shell', 'entrance');
+    });
+});
---
+++ Source/TreasureGuide.Web/Controllers/DurandalController.cs
@@ -0,0 +1,9 @@
+using System.Web.Mvc;
+
+namespace TreasureGuide.Web.Controllers {
+  public class DurandalController : Controller {
+    public ActionResult Index() {
+      return View();
+    }
+  }
+}
