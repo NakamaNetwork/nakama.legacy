c1f1c095385471b1b35ed0fdec0229f7cb56e19d
Bobby Cafazzo <robocafaz@gmail.com>
2017-01-12 21:22:17 -0500

    Moving Durandal Controller to home.

--- Source/TreasureGuide.Web/Controllers/DurandalController.cs
+++
@@ -1,9 +0,0 @@
-using System.Web.Mvc;
-
-namespace TreasureGuide.Web.Controllers {
-  public class DurandalController : Controller {
-    public ActionResult Index() {
-      return View();
-    }
-  }
-}
--- Source/TreasureGuide.Web/Controllers/HomeController.cs
+++ Source/TreasureGuide.Web/Controllers/HomeController.cs
@@ -10,8 +10,6 @@ namespace TreasureGuide.Web.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
-
             return View();
         }
     }
