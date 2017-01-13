039977f1ecd03b1a2dac89f491b71b0de07d2eec
Bobby Cafazzo <robocafaz@gmail.com>
2017-01-12 21:29:08 -0500

    Updating app name.

--- Source/TreasureGuide.Web/App/main.js
+++ Source/TreasureGuide.Web/App/main.js
@@ -16,7 +16,7 @@ define(['durandal/system', 'durandal/app', 'durandal/viewLocator', 'bootstrap'],
     system.debug(true);
     //>>excludeEnd("build");
 
-    app.title = 'Durandal Starter Kit';
+    app.title = 'Nakama';
 
     app.configurePlugins({
         router: true,
