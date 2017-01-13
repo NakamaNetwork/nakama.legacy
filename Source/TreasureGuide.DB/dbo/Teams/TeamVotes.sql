7163d800ed0fe820b3c9f4340349fa5fccd06628
Bobby Cafazzo <robocafaz@gmail.com>
2017-01-12 20:11:20 -0500

    Adding team voting.

---
+++ Source/TreasureGuide.DB/dbo/Teams/TeamVotes.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[TeamVotes]
+(
+    [TeamId] INT NOT NULL,
+    [UserHash] NVARCHAR(256) NOT NULL,
+    CONSTRAINT [PK_dbo.TeamVotes] PRIMARY KEY CLUSTERED ([TeamId] ASC, [UserHash] ASC),
+    CONSTRAINT [FK_dbo.TeamVotes_dbo.Teams] FOREIGN KEY([TeamId]) REFERENCES [dbo].[Teams] ([Id]) ON DELETE CASCADE,
+)
