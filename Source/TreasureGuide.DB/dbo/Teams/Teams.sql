5fd03246f2d44afeaff68b39ea511b62ed51c631
Bobby Cafazzo <bcafazzo@nbi.company>
2017-04-21 12:39:56 -0400

    Updating to new ASP.NET login provider stuff.

--- Source/TreasureGuide.DB/dbo/Teams/Teams.sql
+++ Source/TreasureGuide.DB/dbo/Teams/Teams.sql
@@ -1,9 +1,9 @@
 ﻿CREATE TABLE [dbo].[Teams]
 (
     [Id] INT NOT NULL IDENTITY(1,1),
-    [SubmittedById] NVARCHAR(128) NOT NULL,
+    [SubmittedById] NVARCHAR(450) NOT NULL,
     [Description] NVARCHAR(1000) NULL,
-    [Credits] [nvarchar](250) NULL,
+    [Credits] NVARCHAR(250) NULL,
     CONSTRAINT [PK_dbo.Teams] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK.dbo_Teams_dbo.AspNetUsers] FOREIGN KEY([SubmittedById]) REFERENCES [dbo].[AspNetUsers]([Id])
 )
