09ab5a5806d391b5ac379447eb98d53ec0a9d470
Bobby Cafazzo <robocafaz@gmail.com>
2017-04-23 10:40:29 -0400

    Updating Entity controller.

--- Source/TreasureGuide.Web/Controllers/API/Generic/EntityApiController.cs
+++ Source/TreasureGuide.Web/Controllers/API/Generic/EntityApiController.cs
@@ -1,13 +1,16 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TreasureGuide.Entities;
 using TreasureGuide.Entities.Interfaces;
+using TreasureGuide.Web.Helpers;
 
 namespace TreasureGuide.Web.Controllers.API.Generic
 {
     public class EntityApiController<TKey, TEntity, TStubModel, TDetailModel, TEditorModel> : GenericApiController<TKey, TEntity, TStubModel, TDetailModel, TEditorModel>
         where TKey : struct
-        where TEntity : IIdItem<TKey>
+        where TEntity : class, IIdItem<TKey>
         where TEditorModel : IIdItem<TKey?>
     {
         protected readonly TreasureEntities DbContext;
@@ -19,9 +22,8 @@ namespace TreasureGuide.Web.Controllers.API.Generic
 
         protected override IActionResult Get<TModel>(TKey? id = null)
         {
-            return Do(() => PerformGet(id));
+            return Do(() => PerformGet<TModel>(id));
         }
-
 
         protected override IActionResult Post(TEditorModel model, TKey? id = null)
         {
@@ -33,25 +35,118 @@ namespace TreasureGuide.Web.Controllers.API.Generic
             return Do(() => PerformDelete(id));
         }
 
-        protected object PerformGet(TKey? id)
-        {
-            throw new NotImplementedException();
-        }
-
-        protected object PerformPost(TEditorModel model, TKey? id = null)
-        {
-            throw new NotImplementedException();
-        }
-
-        protected object PerformDelete(TKey? id)
-        {
-            throw new NotImplementedException();
-        }
-
         protected virtual IActionResult Do(Func<object> function)
         {
             var result = function.Invoke();
             return (result as IActionResult) ?? Ok(result);
+        }
+
+        protected virtual object PerformGet<TModel>(TKey? id = null)
+        {
+            var entities = FetchEntities(id);
+            var transformed = typeof(TModel) == typeof(TEntity) ? entities.Cast<TModel>() : Project<TModel>(entities);
+            if (id.HasValue)
+            {
+                var single = transformed.SingleOrDefault();
+                if (single != null)
+                {
+                    return single;
+                }
+                return NotFound(id);
+            }
+            return transformed;
+        }
+
+        protected virtual IQueryable<TEntity> FetchEntities(TKey? id = null)
+        {
+            var queryable = DbContext.Set<TEntity>().AsQueryable();
+            if (id.HasValue)
+            {
+                queryable = queryable.FindId(id);
+            }
+            queryable = Filter(queryable);
+            return queryable;
+        }
+
+        protected virtual object PerformPost(TEditorModel model, TKey? id = null)
+        {
+            if (id.HasValue)
+            {
+                var entities = FetchEntities(id);
+                var single = entities.SingleOrDefault();
+                if (single != null)
+                {
+                    return CreateOrUpdate(model, single);
+                }
+                return NotFound(id);
+            }
+            return BadRequest("No item specified.");
+        }
+
+        protected virtual object PerformDelete(TKey? id)
+        {
+            if (id.HasValue)
+            {
+                var entities = FetchEntities(id);
+                var target = entities.SingleOrDefault();
+                return Remove(target);
+            }
+            return BadRequest("No item specified.");
+        }
+
+        protected virtual async Task<object> CreateOrUpdate(TEditorModel model, TEntity entity)
+        {
+            model = PreProcess(model);
+            var newItem = entity == null;
+            if (newItem)
+            {
+                entity = Create(model);
+                DbContext.Set<TEntity>().Add(entity);
+            }
+            else
+            {
+                entity = Update(model, entity);
+            }
+            entity = PostProcess(entity);
+            await DbContext.SaveChangesAsync();
+            return entity.Id;
+        }
+
+        protected virtual async Task<object> Remove(TEntity single)
+        {
+            DbContext.Set<TEntity>().Remove(single);
+            await DbContext.SaveChangesAsync();
+            return true;
+        }
+
+        protected virtual IQueryable<TModel> Project<TModel>(IQueryable<TEntity> entities)
+        {
+            return entities.Cast<TModel>();
+        }
+
+        protected virtual IQueryable<TEntity> Filter(IQueryable<TEntity> entities)
+        {
+            return entities;
+        }
+
+        protected virtual TEntity Create(TEditorModel model)
+        {
+            return model as TEntity;
+        }
+
+        protected virtual TEntity Update(TEditorModel model, TEntity entity)
+        {
+            return model as TEntity;
+        }
+
+        private TEditorModel PreProcess(TEditorModel model)
+        {
+            return model;
+        }
+
+        private TEntity PostProcess(TEntity entity)
+        {
+            return entity;
         }
     }
 }
