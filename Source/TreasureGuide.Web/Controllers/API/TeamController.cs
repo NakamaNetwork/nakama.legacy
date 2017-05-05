9dbde211fe8e1cc4124393a22a690f5142d0c6a2
Bobby Cafazzo <bcafazzo@nbi.company>
2017-05-05 17:15:15 -0400

    Starting team search.

--- Source/TreasureGuide.Web/Controllers/API/TeamController.cs
+++ Source/TreasureGuide.Web/Controllers/API/TeamController.cs
@@ -1,4 +1,11 @@
-﻿using AutoMapper;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.AspNetCore.Mvc;
 using TreasureGuide.Entities;
 using TreasureGuide.Web.Controllers.API.Generic;
 using TreasureGuide.Web.Models.TeamModels;
@@ -15,6 +22,70 @@ namespace TreasureGuide.Web.Controllers.API
         {
             entity.SubmittedById = "Anonymous";
             return base.PostProcess(entity);
+        }
+
+        [HttpGet]
+        [ActionName("Search")]
+        [Route("[action]")]
+        public async Task<IEnumerable<TeamStubModel>> Search(TeamSearchModel model)
+        {
+            model = model ?? new TeamSearchModel();
+            var teams = DbContext.Teams.AsQueryable();
+
+            teams = SearchTerm(teams, model.Team);
+            teams = SearchStage(teams, model.Stage);
+            teams = SearchLead(teams, model.Leader);
+            teams = SearchGlobal(teams, model.Global);
+            teams = SearchBox(teams, model.MyBox);
+
+            teams = teams.OrderBy(x => x.TeamVotes.Count).Skip(model.PageSize * model.Page).Take(model.Page);
+            var output = teams.ProjectTo<TeamStubModel>(AutoMapper.ConfigurationProvider);
+            return await output.ToListAsync();
+        }
+
+        private IQueryable<Team> SearchTerm(IQueryable<Team> teams, string term)
+        {
+            if (!String.IsNullOrEmpty(term))
+            {
+
+            }
+            return teams;
+        }
+
+        private IQueryable<Team> SearchStage(IQueryable<Team> teams, string stage)
+        {
+            if (!String.IsNullOrEmpty(stage))
+            {
+
+            }
+            return teams;
+        }
+
+        private IQueryable<Team> SearchLead(IQueryable<Team> teams, string leader)
+        {
+            if (!String.IsNullOrEmpty(leader))
+            {
+
+            }
+            return teams;
+        }
+
+        private IQueryable<Team> SearchGlobal(IQueryable<Team> teams, bool global)
+        {
+            if (global)
+            {
+
+            }
+            return teams;
+        }
+
+        private IQueryable<Team> SearchBox(IQueryable<Team> teams, bool myBox)
+        {
+            if (myBox)
+            {
+
+            }
+            return teams;
         }
     }
 }
---
+++ Source/TreasureGuide.Web/Models/TeamModels/TeamSearchModel.cs
@@ -0,0 +1,15 @@
+﻿namespace TreasureGuide.Web.Models.TeamModels
+{
+    public class TeamSearchModel
+    {
+        public string Team { get; set; }
+        public string Leader { get; set; }
+        public string Stage { get; set; }
+
+        public bool MyBox { get; set; }
+        public bool Global { get; set; }
+
+        public int Page { get; set; } = 0;
+        public int PageSize { get; set; } = 25;
+    }
+}
