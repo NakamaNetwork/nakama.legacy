af3f76ef596e269452c57ec1e9b9787fe0d3d3c1
Bobby Cafazzo <robocafaz@gmail.com>
2017-07-01 20:34:47 -0400

    Expanding colo.

--- Source/TreasureGuide.Sniffer/Parsers/StageParser.cs
+++ Source/TreasureGuide.Sniffer/Parsers/StageParser.cs
@@ -12,7 +12,8 @@ namespace TreasureGuide.Sniffer.Parsers
 {
     public class StageParser : TreasureParser<IEnumerable<Stage>>
     {
-        private const string OptcDbStageData = "https://github.com/optc-db/optc-db.github.io/raw/master/common/data/drops.js";
+        private const string OptcDbStageData =
+            "https://github.com/optc-db/optc-db.github.io/raw/master/common/data/drops.js";
 
         public StageParser(TreasureEntities context) : base(context, OptcDbStageData)
         {
@@ -28,7 +29,6 @@ namespace TreasureGuide.Sniffer.Parsers
         protected override IEnumerable<Stage> ConvertData(string trimmed)
         {
             var data = JsonConvert.DeserializeObject<JObject>(trimmed);
-            var i = 0;
             var stages = new List<Stage>();
             foreach (var datum in data)
             {
@@ -40,17 +40,14 @@ namespace TreasureGuide.Sniffer.Parsers
                     {
                         return HandleColiseum(child);
                     }
-                    else
-                    {
-                        var global = child["global"]?.ToString().ToBoolean() ?? false;
-                        return new[] { HandleSingle(name, global, stageType) };
-                    }
+                    var global = child["global"]?.ToString().ToBoolean() ?? false;
+                    return new[] { HandleSingle(name, global, stageType) };
                 }));
             }
 
             return stages;
         }
-        
+
         private Stage HandleSingle(string name, bool global, StageType stageType)
         {
             return new Stage
@@ -65,12 +62,24 @@ namespace TreasureGuide.Sniffer.Parsers
         private IEnumerable<Stage> HandleColiseum(JToken child)
         {
             var stageType = StageType.Coliseum;
-            var exhibition = JsonConvert.DeserializeObject<int[]>(child["Exhibition"].ToString());
-            var underground = JsonConvert.DeserializeObject<int[]>(child["Underground"].ToString());
-            var chaos = JsonConvert.DeserializeObject<int[]>(child["Chaos"].ToString());
+            var exhibition = JsonConvert.DeserializeObject<int[]>(child["Exhibition"].ToString())
+                .SelectMany(x => Enumerable.Range(1, 3)
+                    .Select(y => Tuple.Create(x, $" - Exhibition Stage {y}")));
+
+            var underground = JsonConvert.DeserializeObject<int[]>(child["Underground"].ToString())
+                .SelectMany(x => Enumerable.Range(1, 4)
+                    .Select(y => Tuple.Create(x, $" - Underground Stage {y}")));
+
+            var chaos = JsonConvert.DeserializeObject<int[]>(child["Chaos"].ToString())
+                .SelectMany(x => Enumerable.Range(1, 5)
+                    .Select(y => Tuple.Create(x, $" - Chaos Stage {y}")));
+
             var all = exhibition.Concat(underground).Concat(chaos).Distinct();
-            var units = all.Join(Context.Units, x => x, y => y.Id, (id, unit) => unit);
-            var colo = units.Select(x => HandleSingle("Coliseum: " + x.Name, x.Flags.HasFlag(UnitFlag.Global), stageType))
+            var units = all.Join(Context.Units, x => x.Item1, y => y.Id,
+                (stage, unit) => Tuple.Create(unit, stage.Item2));
+            var colo = units.Select(x =>
+                    HandleSingle($"Coliseum: {x.Item1.Name}{x.Item2}",
+                        x.Item1.Flags.HasFlag(UnitFlag.Global), stageType))
                 .ToList();
             return colo;
         }
@@ -79,9 +88,7 @@ namespace TreasureGuide.Sniffer.Parsers
         {
             Context.Stages.Clear();
             foreach (var stage in items)
-            {
                 Context.Stages.Add(stage);
-            }
             await Context.SaveChangesAsync();
         }
     }
