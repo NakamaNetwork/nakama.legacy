1412f2f5ce37a3864ca4d9da23415c5c2f9cfe6f
Bobby Cafazzo <robocafaz@gmail.com>
2018-01-10 19:54:01 -0500

    Adding markdown tools.

---
+++ Source/TreasureGuide.Web/src/custom-elements/editors/markdown-editor.ts
@@ -0,0 +1,12 @@
+﻿import { bindable, computedFrom, customElement } from 'aurelia-framework';
+
+@customElement('markdown-editor')
+export class MarkdownEditor {
+    @bindable value;
+    @bindable placeholder: string;
+    @bindable editing: boolean = true;
+
+    toggle() {
+        this.editing = !this.editing;
+    }
+}
--- Source/TreasureGuide.Web/src/main.ts
+++ Source/TreasureGuide.Web/src/main.ts
@@ -1,5 +1,6 @@
 import { Aurelia } from 'aurelia-framework'
 import environment from './environment';
+import * as showdown from 'showdown';
 
 export function configure(aurelia: Aurelia) {
     aurelia.use
@@ -18,12 +19,19 @@ export function configure(aurelia: Aurelia) {
         .plugin('aurelia-dialog')
         .plugin('aurelia-validation')
         .plugin('aurelia-plugins-pagination')
+        .plugin('aurelia-markdown')
         // Custom Elements
         .feature('./custom-attributes')
         .feature('./custom-elements')
         .feature('./custom-elements/dialogs')
         .feature('./custom-elements/displays')
+        .feature('./custom-elements/editors')
         .feature('./value-converters');
+
+    showdown.setOption('tables', true);
+    showdown.setOption('strikethrough', true);
+    showdown.setOption('emoji', true);
+    showdown.setOption('emoji', true);
 
     aurelia.start().then(() => aurelia.setRoot());
 }
