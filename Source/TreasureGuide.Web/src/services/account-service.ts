1240488e9bb3aeb064e72b597f50a341f9b0dbf5
Bobby Cafazzo <robocafaz@gmail.com>
2018-01-17 20:22:36 -0500

    Forcing first beat.

--- Source/TreasureGuide.Web/src/services/account-service.ts
+++ Source/TreasureGuide.Web/src/services/account-service.ts
@@ -33,6 +33,7 @@ export class AccountService {
             this.userProfile = deserialized;
             if (this.userProfile) {
                 this.startHeartbeat();
+                this.doHeartbeat();
             }
         }
     }
