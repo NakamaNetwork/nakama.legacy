fa5ca54a7d51f97150423c402d3bd1cf8276c8ca
Bobby Cafazzo <robocafaz@gmail.com>
2018-01-19 11:37:28 -0500

    Redirecting to about on first visit.

--- Source/TreasureGuide.Web/src/views/index.ts
+++ Source/TreasureGuide.Web/src/views/index.ts
@@ -1,32 +1,40 @@
 ﻿import { autoinject } from 'aurelia-framework';
 import { TeamQueryService } from '../services/query/team-query-service';
 import { ITeamStubModel } from '../models/imported';
+import { Router } from 'aurelia-router';
 
 @autoinject
 export class HomePage {
     private teamQueryService: TeamQueryService;
+    private router: Router;
 
     private newLoading: boolean = true;
     private trendingLoading: boolean = true;
     private newTeams: ITeamStubModel[] = [];
     private trendingTeams: ITeamStubModel[] = [];
 
-    constructor(teamQueryService: TeamQueryService) {
+    constructor(teamQueryService: TeamQueryService, router: Router) {
         this.teamQueryService = teamQueryService;
+        this.router = router;
     }
 
     activate(params) {
-        this.teamQueryService.latest().then(x => {
-            this.newTeams = x;
-            this.newLoading = false;
-        }).catch(x => {
-            this.newLoading = false;
-        });
-        this.teamQueryService.trending().then(x => {
-            this.trendingTeams = x;
-            this.trendingLoading = false;
-        }).catch(x => {
-            this.trendingLoading = false;
-        });
+        if (!localStorage['visited']) {
+            localStorage['visited'] = true;
+            this.router.navigateToRoute('about');
+        } else {
+            this.teamQueryService.latest().then(x => {
+                this.newTeams = x;
+                this.newLoading = false;
+            }).catch(x => {
+                this.newLoading = false;
+            });
+            this.teamQueryService.trending().then(x => {
+                this.trendingTeams = x;
+                this.trendingLoading = false;
+            }).catch(x => {
+                this.trendingLoading = false;
+            });
+        }
     }
 }
