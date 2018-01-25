b345c47e4590e6b0ebf431e3d28b5a3f9f14d248
Bobby Cafazzo <robocafaz@gmail.com>
2018-01-25 13:08:04 -0500

    Updating key checks.

--- Source/TreasureGuide.DB/dbo/Tables/Teams/TeamGenericSlots.sql
+++ Source/TreasureGuide.DB/dbo/Tables/Teams/TeamGenericSlots.sql
@@ -6,7 +6,7 @@
     [Class] SMALLINT NOT NULL CONSTRAINT [DF_dbo.TeamGenericSlots_Class] DEFAULT 0,
     [Role] SMALLINT NOT NULL CONSTRAINT [DF_dbo.TeamGenericSlots_Role] DEFAULT 0,
     [Sub] BIT NOT NULL CONSTRAINT [DF_dbo.TeamGenericSlots_Sub] DEFAULT 0,
-    CONSTRAINT [PK_dbo.TeamGenericSlots] PRIMARY KEY CLUSTERED ([TeamId] ASC, [Position] ASC),
+    CONSTRAINT [PK_dbo.TeamGenericSlots] PRIMARY KEY CLUSTERED ([TeamId] ASC, [Position] ASC, [Type] ASC, [Class] ASC, [Role] ASC),
     CONSTRAINT [CK_dbo.TeamGenericSlots_Position] CHECK ([Position] >= 0 AND [Position] < 6),
     CONSTRAINT [FK_dbo.TeamGenericSlots_dbo.Teams] FOREIGN KEY([TeamId]) REFERENCES [dbo].[Teams] ([Id]) ON DELETE CASCADE
 )
--- Source/TreasureGuide.DB/dbo/Tables/Teams/TeamUnits.sql
+++ Source/TreasureGuide.DB/dbo/Tables/Teams/TeamUnits.sql
@@ -5,7 +5,7 @@
     [Position] TINYINT NOT NULL,
     [Special] BIT NOT NULL CONSTRAINT [DF_dbo.TeamUnits_Special] DEFAULT 0,
     [Sub] BIT NOT NULL CONSTRAINT [DF_dbo.TeamUnits_Sub] DEFAULT 0,
-    CONSTRAINT [PK_dbo.TeamUnits] PRIMARY KEY CLUSTERED ([TeamId] ASC, [UnitId] ASC, [Position] ASC),
+    CONSTRAINT [PK_dbo.TeamUnits] PRIMARY KEY CLUSTERED ([TeamId] ASC, [Position] ASC, [UnitId] ASC),
     CONSTRAINT [CK_dbo.TeamUnits_Position] CHECK ([Position] >= 0 AND [Position] < 6),
     CONSTRAINT [FK_dbo.TeamUnits_dbo.Teams] FOREIGN KEY([TeamId]) REFERENCES [dbo].[Teams] ([Id]) ON DELETE CASCADE,
     CONSTRAINT [FK_dbo.TeamUnits_dbo.Units] FOREIGN KEY([UnitId]) REFERENCES [dbo].[Units] ([Id]) ON DELETE CASCADE
