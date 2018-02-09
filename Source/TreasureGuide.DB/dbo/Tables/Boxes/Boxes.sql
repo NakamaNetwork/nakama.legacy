d67517ac46482bf8aecd5ba3db0df9f00f58a9b9
Bobby Cafazzo <robocafaz@gmail.com>
2018-02-09 15:05:03 -0500

    Adding blacklist option and fixing missed sql imports.

--- Source/TreasureGuide.DB/dbo/Tables/Boxes/Boxes.sql
+++ Source/TreasureGuide.DB/dbo/Tables/Boxes/Boxes.sql
@@ -5,6 +5,7 @@
     [Name] NVARCHAR(250) NOT NULL,
     [FriendId] NUMERIC(9) NULL,
     [Global] BIT NULL,
+    [Blacklist] BIT NULL,
     [Public] BIT NULL CONSTRAINT [DF_dbo.Boxes_Public] DEFAULT 1,
     CONSTRAINT [PK_dbo.Boxes] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK.dbo_Boxes_dbo.UserProfiles] FOREIGN KEY([UserId]) REFERENCES [dbo].[UserProfiles]([Id]) ON DELETE CASCADE
