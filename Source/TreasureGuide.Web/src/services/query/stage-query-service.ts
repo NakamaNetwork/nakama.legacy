772c5b92373fc6c30f8dc56bde4a24e57dfba9d4
Bobby Cafazzo <robocafaz@gmail.com>
2018-02-28 00:15:13 -0500

    Fixing up stages.

--- Source/TreasureGuide.Web/src/services/query/stage-query-service.ts
+++ Source/TreasureGuide.Web/src/services/query/stage-query-service.ts
@@ -1,7 +1,7 @@
 ﻿import { autoinject } from 'aurelia-framework';
 import { HttpEngine } from '../../tools/http-engine';
 import { SearchModel } from '../../models/search-model';
-import { IStageStubModel, IStageSearchModel, SearchConstants } from '../../models/imported';
+import { IStageStubModel, IStageSearchModel, SearchConstants, StageType } from '../../models/imported';
 import { LocallySearchedQueryService } from './generic/locally-searched-query-service';
 
 @autoinject
@@ -11,17 +11,47 @@ export class StageQueryService extends LocallySearchedQueryService<number, IStag
     }
 
     protected performSearch(items: IStageStubModel[], searchModel: StageSearchModel): IStageStubModel[] {
+        items = this.searchTerm(items, searchModel.term);
+        items = this.searchType(items, searchModel.type);
+        items = this.searchGlobal(items, searchModel.global);
+        return items;
+    }
+
+    protected searchTerm(items: IStageStubModel[], term: string): IStageStubModel[] {
+        return this.doTermSearch(items, x => x.aliases.concat(x.name), term);
+    }
+
+    protected searchType(items: IStageStubModel[], type: StageType): IStageStubModel[] {
+        if (type) {
+            items = items.filter(x => x.type === type);
+        }
+        return items;
+    }
+
+    protected searchGlobal(items: IStageStubModel[], global: boolean): IStageStubModel[] {
+        if (global) {
+            items = items.filter(x => x.global);
+        }
         return items;
     }
 
     protected performSort(items: IStageStubModel[], sortBy: string, sortDesc: boolean): IStageStubModel[] {
-        return items;
+        switch (sortBy) {
+            case SearchConstants.SortName:
+                return this.doSort(items, [x => x.name, x => x.unitId], [sortDesc, false]);
+            case SearchConstants.SortType:
+                return this.doSort(items, [x => x.type, x => x.name], [sortDesc, false]);
+            case SearchConstants.SortCount:
+                return this.doSort(items, [x => x.teamCount, x => x.name, x => x.unitId], [sortDesc, false, false]);
+            default:
+                return this.doSort(items, [x => x.name, x => x.unitId], [false]);
+        }
     }
 }
 
 export class StageSearchModel extends SearchModel implements IStageSearchModel {
     term: string = '';
-    type: number;
+    type: StageType;
     global: boolean = false;
     cacheKey: string = 'search-stage';
 
