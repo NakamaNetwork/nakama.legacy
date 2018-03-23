ac707a5c64339489a41958cfcf27a22bdb13b9b0
Bobby Cafazzo <robocafaz@gmail.com>
2018-03-23 00:20:44 -0400

    Fixing bad SQL.

--- Source/TreasureGuide.DB/dbo/Tables/Schedule/ScheduledEvents.sql
+++ Source/TreasureGuide.DB/dbo/Tables/Schedule/ScheduledEvents.sql
@@ -2,7 +2,7 @@
 (
     [StageId] INT NOT NULL,
     [Global] BIT NOT NULL,
-    [StartDate] DATE(7) NOT NULL,
+    [StartDate] DATETIMEOFFSET(7) NOT NULL,
     [EndDate] DATETIMEOFFSET(7) NOT NULL,
     CONSTRAINT [PK_dbo.ScheduledEvents] PRIMARY KEY CLUSTERED ([StageId] ASC, [Global] ASC, [StartDate] ASC, [EndDate] ASC),
     CONSTRAINT [FK_dbo.ScheduledEvents_dbo.Stages] FOREIGN KEY([StageId]) REFERENCES [dbo].[Stages] ([Id]) ON DELETE CASCADE
