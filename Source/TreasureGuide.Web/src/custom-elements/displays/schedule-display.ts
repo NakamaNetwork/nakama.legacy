fd74b33eb8c5e8c1924b5546fd30a88b80572efc
Bobby Cafazzo <robocafaz@gmail.com>
2018-03-23 00:12:25 -0400

    Moving schedule to custom element.

--- Source/TreasureGuide.Web/src/custom-elements/displays/schedule-display.ts
+++ Source/TreasureGuide.Web/src/custom-elements/displays/schedule-display.ts
@@ -1,24 +1,57 @@
-import { autoinject, bindable, computedFrom, customElement } from 'aurelia-framework';
+import { autoinject, bindable, computedFrom, customElement, BindingEngine } from 'aurelia-framework';
+import { StageQueryService } from '../../services/query/stage-query-service';
+import { IScheduleModel, IStageStubModel } from '../../models/imported';
+import { ArrayHelper } from '../../tools/array-helper';
 
 @autoinject
 @customElement('schedule-display')
 export class ScheduleDisplay {
-    @bindable schedule: any[] = [];
-    @bindable upcoming: boolean;
-    @bindable global: boolean;
+    private bindingEngine: BindingEngine;
+    private stageQueryService: StageQueryService;
 
-    @computedFrom('upcoming')
-    get header() {
-        return this.upcoming ? 'Upcoming' : 'Live';
+    private scheduleLoading: boolean = true;
+    private fullSchedule: IScheduleModel;
+    private currentSchedule = [];
+    private upcomingSchedule = [];
+    private globalSchedule: boolean = true;
+
+    private globalKey: string = 'global_schedule';
+
+    constructor(stageQueryService: StageQueryService, bindingEngine: BindingEngine) {
+        this.stageQueryService = stageQueryService;
+        this.bindingEngine = bindingEngine;
+
+        var global = localStorage.getItem(this.globalKey);
+        if (global) {
+            this.globalSchedule = JSON.parse(global);
+        }
+        this.refreshSchedule();
+        this.bindingEngine.propertyObserver(this, 'globalSchedule').subscribe((n, o) => {
+            localStorage.setItem(this.globalKey, JSON.stringify(n));
+            this.refreshSchedule();
+        });
     }
 
-    @computedFrom('upcoming')
-    get unitSize() {
-        return this.upcoming ? 'tiny' : 'small';
+    refreshSchedule() {
+        this.scheduleLoading = true;
+        this.stageQueryService.schedule().then(x => {
+            this.fullSchedule = x;
+            this.stageQueryService.get().then(y => {
+                this.currentSchedule = this.getEvents(x.live, this.globalSchedule, y);
+                this.upcomingSchedule = this.getEvents(x.upcoming, this.globalSchedule, y);
+                this.scheduleLoading = false;
+            }).catch(x => {
+                this.scheduleLoading = false;
+            });
+        }).catch(x => {
+            this.scheduleLoading = false;
+        });
     }
 
-    @computedFrom('upcoming')
-    get scheduleClass() {
-        return (this.upcoming ? 'upcoming' : 'live') + ' ' + (this.global ? 'global' : 'japan');
+    getEvents(eventData, global: boolean, stages: IStageStubModel[]): any[] {
+        var source = global ? eventData.global : eventData.japan;
+        var mapping = stages = source.map(x => stages.find(y => y.id == x));
+        var bins = ArrayHelper.binBy(mapping, 'type');
+        return bins;
     }
 }
--- Source/TreasureGuide.Web/src/views/index.ts
+++ Source/TreasureGuide.Web/src/views/index.ts
@@ -1,22 +1,12 @@
-﻿import { autoinject, BindingEngine } from 'aurelia-framework';
+﻿import { autoinject } from 'aurelia-framework';
 import { TeamQueryService } from '../services/query/team-query-service';
-import { ITeamStubModel, IScheduleModel, IStageStubModel } from '../models/imported';
+import { ITeamStubModel } from '../models/imported';
 import { Router } from 'aurelia-router';
-import { StageQueryService } from '../services/query/stage-query-service';
-import { ArrayHelper } from '../tools/array-helper';
 
 @autoinject
 export class HomePage {
     private teamQueryService: TeamQueryService;
-    private stageQueryService: StageQueryService;
     private router: Router;
-    private bindingEngine: BindingEngine;
-
-    private scheduleLoading: boolean = true;
-    private fullSchedule: IScheduleModel;
-    private currentSchedule = [];
-    private upcomingSchedule = [];
-    private globalSchedule: boolean = true;
 
     private newLoading: boolean = true;
     private newTeams: ITeamStubModel[] = [];
@@ -24,13 +14,9 @@ export class HomePage {
     private trendingLoading: boolean = true;
     private trendingTeams: ITeamStubModel[] = [];
 
-    private globalKey: string = 'global_schedule';
-
-    constructor(teamQueryService: TeamQueryService, stageQueryService: StageQueryService, bindingEngine: BindingEngine, router: Router) {
+    constructor(teamQueryService: TeamQueryService, router: Router) {
         this.teamQueryService = teamQueryService;
-        this.stageQueryService = stageQueryService;
         this.router = router;
-        this.bindingEngine = bindingEngine;
     }
 
     activate(params) {
@@ -50,40 +36,6 @@ export class HomePage {
             }).catch(x => {
                 this.trendingLoading = false;
             });
-
-
-            var global = localStorage.getItem(this.globalKey);
-            if (global) {
-                this.globalSchedule = JSON.parse(global);
-            }
-            this.refreshSchedule();
-            this.bindingEngine.propertyObserver(this, 'globalSchedule').subscribe((n, o) => {
-                localStorage.setItem(this.globalKey, JSON.stringify(n));
-                this.refreshSchedule();
-            });
         }
-    }
-
-    refreshSchedule() {
-        this.scheduleLoading = true;
-        this.stageQueryService.schedule().then(x => {
-            this.fullSchedule = x;
-            this.stageQueryService.get().then(y => {
-                this.currentSchedule = this.getEvents(x.live, this.globalSchedule, y);
-                this.upcomingSchedule = this.getEvents(x.upcoming, this.globalSchedule, y);
-                this.scheduleLoading = false;
-            }).catch(x => {
-                this.scheduleLoading = false;
-            });
-        }).catch(x => {
-            this.scheduleLoading = false;
-        });
-    }
-
-    getEvents(eventData, global: boolean, stages: IStageStubModel[]): any[] {
-        var source = global ? eventData.global : eventData.japan;
-        var mapping = stages = source.map(x => stages.find(y => y.id == x));
-        var bins = ArrayHelper.binBy(mapping, 'type');
-        return bins;
     }
 }
