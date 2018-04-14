2f969c5dcb1feb2f974ea356406e6e58da365e33
Bobby Cafazzo <robocafaz@gmail.com>
2018-04-13 21:28:08 -0400

    Updating GCR sql logic.

--- Source/TreasureGuide.DB/PostDeploy/GCRPopulation.sql
+++ Source/TreasureGuide.DB/PostDeploy/GCRPopulation.sql
@@ -11,3 +11,5 @@ INSERT INTO [dbo].[GCRStages]
     SELECT [Id], [Id] FROM [dbo].[Stages]
         WHERE ([Type] = 4 AND [Name] NOT LIKE '%!?%')
         OR ([Type] = 5 AND [Id] LIKE '%1')
+        OR ([Type] = 6 AND [Name] LIKE 'Invasion%')
+        OR ([Type] = 7)
--- Source/TreasureGuide.DB/dbo/Views/GCR/GCRTable.sql
+++ Source/TreasureGuide.DB/dbo/Views/GCR/GCRTable.sql
@@ -1,16 +1,52 @@
 ﻿CREATE VIEW [dbo].[GCRTable]
     AS
 SELECT
-    G.[Id],
-    U.[UnitId] AS [LeaderId],
-    G.[StageId],
-    G.[F2P],
-    G.[Global],
-    G.[Video]
+    [Id],
+    [LeaderId],
+    [StageId],
+    [F2P],
+    [Global],
+    [Video]
 FROM
-    [dbo].[GCRSummaries] AS G
-    JOIN [dbo].[GCRStages] AS S
-        ON G.[StageId] = S.[StageId]
-    LEFT JOIN [dbo].[GCRUnits] AS U
-        ON G.[LeaderId] = U.[UnitId]
-    WHERE U.[UnitId] IS NOT NULL OR [F2P] = 1
+    (SELECT
+        *,
+        ROW_NUMBER() OVER (PARTITION BY [LeaderId], [StageId] ORDER BY [Score] DESC) AS [Rank]
+    FROM
+        (SELECT
+            *,
+            CAST(
+                (CASE WHEN [Global] = 1 THEN 3 ELSE 0 END) +
+                (CASE WHEN [F2P] = 1 THEN 2 ELSE 0 END) +
+                (CASE WHEN [Video] = 1 THEN 1 ELSE 0 END)
+            AS INT) AS [Score]
+        FROM 
+            (SELECT
+                T.[Id],
+                GCRU.[UnitId] AS [LeaderId],
+                T.[StageId],
+                CAST(CASE WHEN SUM(CASE WHEN U.[Position] = 0 OR (SU.[Flags] & 2 = 0 AND SU.[Flags] & 4 = 0 AND SU.[Flags] & 8 = 0) THEN 1 ELSE 0 END) = COUNT(U.[UnitId]) THEN 1 ELSE 0 END AS BIT) AS [F2P],
+                CAST(CASE WHEN SUM(CASE WHEN SU.[Flags] & 1 = 1 THEN 1 ELSE 0 END) = COUNT(U.[UnitId]) AND LU.[Flags] & 1 = 1 THEN 1 ELSE 0 END AS BIT) AS [Global],
+                CAST(CASE WHEN COUNT(V.[Id]) > 0 THEN 1 ELSE 0 END AS BIT) AS [Video]
+            FROM [dbo].[Teams] AS T
+                JOIN [dbo].[GCRStages] AS GCRS
+                    ON GCRS.[StageId] = T.[StageId]
+                JOIN [dbo].[TeamUnits] AS L
+                    ON T.[Id] = L.[TeamId] AND L.[Position] = 1 AND L.[Sub] = 0
+                LEFT JOIN [dbo].[Units] AS LU
+                    ON L.[UnitId] = LU.[Id]
+                LEFT JOIN [dbo].[GCRUnits] AS GCRU
+                    ON GCRU.[UnitId] = LU.[Id]
+                LEFT JOIN [dbo].[TeamUnits] AS U
+                    ON T.[Id] = U.[TeamId] AND U.[Position] <> 1 AND U.[Sub] = 0
+                LEFT JOIN [dbo].[Units] AS SU
+                    ON U.[UnitId] = SU.[Id]
+                LEFT JOIN [dbo].[TeamVideos] AS V
+                    ON T.[Id] = V.[TeamId]
+                WHERE T.[StageId] IS NOT NULL AND T.[Draft] = 0 AND T.[Deleted] = 0 AND ISNULL(V.[Deleted], 0) = 0
+                GROUP BY T.[Id], T.[StageId], GCRU.[UnitId], LU.[Flags]
+            ) GCR
+        WHERE [LeaderId] IS NOT NULL OR [F2P] = 1
+    ) Scores
+    WHERE [Score] > 0
+) Grouped
+WHERE [Rank] = 1
