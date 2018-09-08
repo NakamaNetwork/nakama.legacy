c9a30ff8c4470261251e9a7aadc370bc02b63413
Bobby Cafazzo <robocafaz@gmail.com>
2018-09-08 16:33:42 -0400

    Adding team comment models.

---
+++ Source/TreasureGuide.Common/Models/TeamModels/TeamCommentModels.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using TreasureGuide.Entities;
+
+namespace TreasureGuide.Common.Models.TeamModels
+{
+    public class TeamCommentStubModel
+    {
+        public int Id { get; set; }
+        public int TeamId { get; set; }
+
+        [StringLength(4000)]
+        public string Text { get; set; }
+    }
+
+    public class TeamCommentDetailModel : TeamCommentStubModel
+    {
+        public bool Deleted { get; set; }
+        public bool Reported { get; set; }
+
+        public int Score { get; set; }
+
+        public string SubmittedById { get; set; }
+        public string SubmittedByName { get; set; }
+        public int? SubmittedByUnitId { get; set; }
+        public bool SubmittedByIsDonor { get; set; }
+        public DateTimeOffset? EditedDate { get; set; }
+    }
+
+    public class TeamCommentEditorModel : TeamCommentStubModel
+    {
+    }
+}
---
+++ Source/TreasureGuide.Common/Models/TeamModels/TeamCommentSearchModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TreasureGuide.Common.Models.TeamModels
+{
+    public class TeamCommentSearchModel : SearchModel
+    {
+        public bool Deleted { get; set; }
+        public bool Reported { get; set; }
+    }
+}
