e957a3ec808554c069de8a549ccbf9ea0c794d7a
Bobby Cafazzo <robocafaz@gmail.com>
2018-11-06 20:47:16 -0500

    Adding notification schema.

---
+++ Source/TreasureGuide.DB/dbo/Tables/Notifications/Triggers/TeamCommentTrigger.sql
@@ -0,0 +1,15 @@
+﻿CREATE TRIGGER [TeamCommentTrigger]
+    ON [dbo].[TeamComments]
+    FOR INSERT
+    AS
+    BEGIN
+        SET NOCOUNT ON
+        INSERT INTO [dbo].[Notifications]([UserId], [EventType], [EventId], [TriggerUserId])
+            SELECT 
+                T.[SubmittedById], 1, T.[Id], I.[SubmittedById]
+            FROM INSERTED AS I
+            JOIN [dbo].[Teams] AS T
+                ON I.[TeamId] = T.[Id]
+            WHERE T.[SubmittedById] != I.[SubmittedById]
+    END
+GO
