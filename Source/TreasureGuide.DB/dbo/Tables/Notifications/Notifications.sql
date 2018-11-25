4930b371fd302cd251e26b9dc57d83cd4bb3aa06
Bobby Cafazzo <robocafaz@gmail.com>
2018-11-25 10:11:09 -0500

    Removing notification summary and trigger user id for performance reasons.

--- Source/TreasureGuide.DB/dbo/Tables/Notifications/Notifications.sql
+++ Source/TreasureGuide.DB/dbo/Tables/Notifications/Notifications.sql
@@ -4,11 +4,9 @@
     [UserId] NVARCHAR(450) NOT NULL,
     [EventType] INT NOT NULL,
     [EventId] INT NULL,
-    [TriggerUserId] NVARCHAR(450) NULL,
     [ReceivedDate] DATETIMEOFFSET(7) NOT NULL CONSTRAINT [DF_dbo.Notifications_ReceivedDate] DEFAULT SYSDATETIMEOFFSET(),
     CONSTRAINT [PK_dbo.Notifications] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK.dbo_Notifications_dbo.UserProfiles] FOREIGN KEY([UserId]) REFERENCES [dbo].[UserProfiles]([Id]),
-    CONSTRAINT [FK.dbo_Notifications_dbo.UserProfiles_Trigger] FOREIGN KEY([TriggerUserId]) REFERENCES [dbo].[UserProfiles]([Id])
+    CONSTRAINT [FK.dbo_Notifications_dbo.UserProfiles] FOREIGN KEY([UserId]) REFERENCES [dbo].[UserProfiles]([Id])
 )
 GO
 -- EventType 0 = Unknown, 1 = Team Comment, 2 = Comment Reply, 3 = Video Submission
