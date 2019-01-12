24eba87edccb33efacd2c500f370433c82bccfc7
Bobby Cafazzo <robocafaz@gmail.com>
2019-01-12 14:57:00 -0500

    Updating default data.

--- Source/TreasureGuide.DB/PostDeploy/dbo.Ships.Table.sql
+++ Source/TreasureGuide.DB/PostDeploy/dbo.Ships.Table.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a0142a56ab157bda10c54684b7ea97951dee2a9eef8e13af0d8ad01166e3ef3e
+oid sha256:23cd14b4f1e8e09ba6b0cff7f679e079dd869c825212e7124e6071531eba14e4
 size 20180
