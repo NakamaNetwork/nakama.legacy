2f23d5f187cda632f316e946a565f95e41ea8033
Bobby Cafazzo <robocafaz@gmail.com>
2019-01-30 20:40:16 -0500

    Updating schedule parser.

--- Source/TreasureGuide.Sniffer/DataParser/ScheduleParserCal.cs
+++ Source/TreasureGuide.Sniffer/DataParser/ScheduleParserCal.cs
@@ -247,6 +247,10 @@ namespace TreasureGuide.Sniffer.DataParser
                     return 2204400;
                 case 2144:
                     return 2214600;
+                case 1963: // Sanji/Pudding Wedding FN
+                    return 6196100;
+                case 9999: // Shanks and Mihawk
+                    return 4251300;
                 case 530: // 3rd anni champ challenge
                 case 562:
                 case 1045:
