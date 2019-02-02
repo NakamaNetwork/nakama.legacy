d111f9b79ef8821a4a9c3f2b39202f76647ec533
Bobby Cafazzo <robocafaz@gmail.com>
2019-02-02 13:02:26 -0500

    Adding new DB solution.

---
+++ Source/NakamaNetwork.DB/dbo/Tables/Accounts/UserProfiles.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[UserProfiles]
+(
+    [Id] NVARCHAR(450) NOT NULL,
+    [UserName] NVARCHAR(256) NOT NULL,
+    [FriendId] NUMERIC(9) NULL,
+    [Website] NVARCHAR(200) NULL,
+    [UnitId] INT NULL,
+    [Global] BIT NULL, 
+    CONSTRAINT [PK_dbo.UserProfile] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK.dbo_UserProfile_dbo.AspNetUsers] FOREIGN KEY([Id]) REFERENCES [dbo].[AspNetUsers]([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK.dbo_UserProfile_dbo.Units] FOREIGN KEY([UnitId]) REFERENCES [dbo].[Units]([Id]) ON DELETE SET NULL
+)
